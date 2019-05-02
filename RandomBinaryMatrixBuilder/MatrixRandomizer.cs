21deacd1cae97f0d9c887ea7b70db719888596d2
Rik Riesmeijer <rmriesmeijer@gmail.com>
2019-05-02 20:27:00 +0200

    Functional codebase

--- RandomBinaryMatrixBuilder/MatrixRandomizer.cs
+++ RandomBinaryMatrixBuilder/MatrixRandomizer.cs
@@ -27,16 +27,19 @@ namespace RandomBinaryMatrixBuilder
             // List of the 1's in general and per row.
             Stack<int>[] rowOnes = new Stack<int>[m];
             HashSet<int>[] rowOnesSet = new HashSet<int>[m];
+            int[] rowsums = new int[m];
             Stack<Index> ones = new Stack<Index>();
             HashSet<Index> onesSet = new HashSet<Index>();
             for (int j = 0; j < m; j++)
             {
                 rowOnes[j] = new Stack<int>();
+                rowOnesSet[j] = new HashSet<int>();
                 for (int i = 0; i < n; i++)
                     if (matrix[i, j] == 1)
                     {
                         rowOnes[j].Push(i);
                         rowOnesSet[j].Add(i);
+                        rowsums[j]++;
                         ones.Push(new Index(i, j));
                         onesSet.Add(new Index(i, j));
                     }
@@ -48,6 +51,7 @@ namespace RandomBinaryMatrixBuilder
             for (int i = 0; i < n; i++)
             {
                 feasableRowsForColumn[i] = new Stack<int>();
+                feasableRowsForColumnSet[i] = new HashSet<int>();
                 for (int j = 0; j < m; j++)
                     if (matrix[i, j] == 0 && rowOnes[j].Count > 0)
                     {
@@ -62,14 +66,99 @@ namespace RandomBinaryMatrixBuilder
                 feasableRowsForColumn[i].Shuffle();
             for (int j = 0; j < m; j++)
                 rowOnes[j].Shuffle();
+            ones.Shuffle();
 
             // Get some random cycles.
             List<Stack<Index>> cycles = new List<Stack<Index>>();
             HashSet<int> visitedColumns = new HashSet<int>();
+            HashSet<Index> visitedOnes = new HashSet<Index>();
+            Stack<Index> currentPath = new Stack<Index>();
+            while (onesSet.Count > 0)
+            {
+                Index current = ones.Pop();
+                if (currentPath.Count == 0)
+                {
+                    visitedColumns = new HashSet<int>();
+                    while (ones.Count > 0 && visitedOnes.Contains(current))
+                        current = ones.Pop();
+
+                    visitedColumns.Add(current.i);
+                    currentPath.Push(current);
+                }
+                else
+                {
+                    Index last = currentPath.Pop();
+                    if (random.Next(0, feasableRowsForColumnSet[last.i].Count + 1) != 0)
+                    {
+                        currentPath.Push(last);
+                        while (feasableRowsForColumn[last.i].Count > 0 && visitedOnes.Contains(current))
+                        {
+                            int nextrow = feasableRowsForColumn[last.i].Pop();
+                            while (feasableRowsForColumn[last.i].Count > 0 && !feasableRowsForColumn[last.i].Contains(current.j))
+                                nextrow = feasableRowsForColumn[last.i].Pop();
+
+                            int nextcolumn = rowOnes[nextrow].Pop();
+                            if(rowOnes[nextrow].Count == 0)
+                                for (int i = 0; i < n; i++)
+                                    feasableRowsForColumnSet[i].Remove(nextrow);
+                            current = new Index(nextrow, nextcolumn);
+                        }
+                        currentPath.Push(current);
+                        if(!visitedColumns.Contains(current.i))
+                            visitedColumns.Add(current.i);
+                        else
+                        {
+                            // Build cycle and store it.
+                            last = currentPath.Pop();
+                            Stack<Index> cycle = new Stack<Index>();
+                            cycle.Push(last);
+                            Index top = currentPath.Pop();
+                            while(top.i != last.i)
+                            {
+                                cycle.Push(top);
+                                visitedColumns.Remove(top.i);
+                                if (currentPath.Count > 0)
+                                    top = currentPath.Pop();
+                                else
+                                    top.i = last.i;
+                            }
+                            cycles.Add(cycle);
+                        }
+                    }
+                    else
+                    {
+                        visitedColumns.Remove(last.i);
+                    }
+                }
+                visitedOnes.Add(current);
+                onesSet.Remove(current);
+            }
             
 
             // Apply transformations for final matrix.
             int[,] finalmatrix = new int[n, m];
+            HashSet<Index> changes = new HashSet<Index>();
+            foreach(Stack<Index> cycle in cycles)
+            {
+                Index first = cycle.Pop();
+                changes.Add(first);
+                Index last = first;
+                Index current;
+                while(cycle.Count > 0)
+                {
+                    current = cycle.Pop();
+                    changes.Add(new Index(current.i, last.j));
+                    changes.Add(current);
+                    last = current;
+                }
+            }
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < m; j++)
+                {
+                    finalmatrix[i, j] = matrix[i, j];
+                    if(changes.Contains(new Index(i, j)))
+                        finalmatrix[i, j] ^= 1;
+                }
 
             return finalmatrix;
         }
--- RandomBinaryMatrixBuilder/Program.cs
+++ RandomBinaryMatrixBuilder/Program.cs
@@ -11,8 +11,8 @@ namespace RandomBinaryMatrixBuilder
         static void Main(string[] args)
         {
             // Build matrix to test with randomly.
-            int n = 100;
-            int m = 10000;
+            int n = 500;
+            int m = 500;
             int[,] testmatrix = TestMatrixGenerator.GetMatrix(n, m);
 
             // Randomize matrix.
@@ -20,9 +20,42 @@ namespace RandomBinaryMatrixBuilder
 
             // Count differences.
             int count = 0;
-            for(int i = 0; i < n; i++)
-                for(int j = 0; j < m; j++)
+            int[] columnsums = new int[n];
+            int[] rowsums = new int[m];
+            int[] columnsumsrandomized = new int[n];
+            int[] rowsumsrandomized = new int[m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
                     count = testmatrix[i, j] == randomizedmatrix[i, j] ? count : count + 1;
+                    if(testmatrix[i,j] == 1)
+                    {
+                        columnsums[i]++;
+                        rowsums[j]++;
+                        columnsumsrandomized[i]++;
+                        rowsumsrandomized[j]++;
+                    }
+
+                }
+
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                if (columnsums[i] != columnsumsrandomized[i])
+                    Console.WriteLine("Auwas row");
+                else
+                {
+                    Console.WriteLine("hmm" + i);
+                }
+            }
+            for (int j = 0; j < m; j++)
+            {
+                if (rowsums[j] != rowsumsrandomized[j])
+                    Console.WriteLine("Auwas column");
+            }
 
             // Output result.
             Console.WriteLine(count);
